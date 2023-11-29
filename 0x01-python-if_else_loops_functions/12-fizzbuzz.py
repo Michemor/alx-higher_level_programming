76157ebefff9afd4bab17b91e948cb0d63db417a
Michemor <yatoijema@gmail.com>
2023-11-29 03:05:42 +0300

    check for 100

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,7 +8,7 @@ def fizzbuzz():
     for n in range(1, 101):
         if n % 3 == 0:
             print("Fizz", end=" ")
-        elif n % 5 == 0:
+        elif n % 5 == 0 and n != 100:
             print("Buzz", end=" ")
         elif n % 3 == 0 and n % 5 == 0:
             print("FizzBuzz", end=" ")
