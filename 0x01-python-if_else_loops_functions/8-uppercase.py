4f86c57f3ec38a834256289f3ea16eecf058f34a
Michemor <yatoijema@gmail.com>
2023-11-29 03:24:22 +0300

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    """
+    function converts string to uppercase
+    Input: str
+    Output: prints string
+    """
+    for n in str:
+        if ord(n)>=97 and ord(n)<=122:
+            n = chr(ord(n) - 32)
+        print("{}".format(n), end="")
+    print("")
