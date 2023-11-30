acc8b36570eee02adabe66edec039e99e4ca3bbe
Michemor <yatoijema@gmail.com>
2023-11-30 23:59:04 +0300

    used renamig of imported module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0
+import add_0 as plus
+
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{0} + {1} = {2}".format(a, b, add_0.add(a, b)), end="\n")
+    print("{0} + {1} = {2}".format(a, b, plus.add(a, b)), end="\n")
