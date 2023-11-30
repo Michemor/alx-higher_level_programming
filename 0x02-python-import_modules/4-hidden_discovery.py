0e9ef2b2ae617471615e0161d652ef9f5ec50807
Michemor <yatoijema@gmail.com>
2023-12-01 02:33:36 +0300

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    
-    names = dir(hidden_4)
-    for name in names:
+    name_list = dir(hidden_4)
+    for name in name_list:
         if name[:2] != "__":
-            print("{names}".format(names))
-
+            print("{}".format(name))
