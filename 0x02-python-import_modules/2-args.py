541df6e309737497e7c6cc476f600a7892ceedd6
Michemor <yatoijema@gmail.com>
2023-12-01 01:26:45 +0300

    imported specific function from sys

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
-    import sys
+if __name__ == "__main__":
+    from sys import argv
 
-    ac = (len(sys.argv) - 1)
-    argv = sys.argv
+    ac = len(argv) - 1
+    
     if ac == 0:
         print("{} arguments.".format(ac), end="\n")
     elif ac == 1:
-        print("{} argument".format(ac), end="\n")
-        print("{}: {}".format(ac, argv[1]), end="\n")
+        print("1 argument".format(), end="\n")
+        print("1: {}".format(argv[1]), end="\n")
     elif ac > 1:
         print("{} arguments:".format(ac), end="\n")
-        for i in range(1, (ac + 1)):
+        for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]), end="\n")
