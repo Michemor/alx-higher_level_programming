c05f06577b5e44e22ab134d686faf473d339d6d2
Michemor <yatoijema@gmail.com>
2023-12-03 17:48:21 +0300

    modified 3-print... Replace in a copy

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """reverses contents of a list and prints them"""
-    for i in range(len(my_list) - 1, -1, -1):
+    rev_len = len(my_list) - 1
+    for i in range(rev_len, -1, -1):
         print("{:d}".format(my_list[i]))
---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """replaces element without modifying original list"""
+    new_list = my_list.copy()
+    if idx < 0:
+        return (new_list)
+    elif idx > len(new_list):
+        return (new_list)
+    for i in range(len(new_list)):
+        if i == idx:
+            new_list[i] = element
+            return (new_list)
