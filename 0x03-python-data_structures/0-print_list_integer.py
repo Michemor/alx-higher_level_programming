9475463f7debe701a267f36301dfa04ad7632206
Michemor <yatoijema@gmail.com>
2023-12-03 16:43:17 +0300

    correction on printing using .format()

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -6,4 +6,4 @@ def print_list_integer(my_list=[]):
     Output: integers printed line by line
     """
     for i in range(len(my_list)):
-        print("{}".format(my_list[i]), end='\n')
+        print("{:d}".format(my_list[i]))
