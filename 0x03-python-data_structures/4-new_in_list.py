d305ab68bcfb461496c90ba81648e8e4fc34d3be
Michemor <yatoijema@gmail.com>
2023-12-03 21:13:28 +0300

    Can you C me now?

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx < 0:
         return (new_list)
-    elif idx > len(new_list):
+    elif idx >= len(new_list):
         return (new_list)
     for i in range(len(new_list)):
         if i == idx:
