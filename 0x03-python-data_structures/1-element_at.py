07f673c2e6f39c783396a9a9a8ec2da929ae9dc6
Michemor <yatoijema@gmail.com>
2023-12-03 17:11:30 +0300

    limit includes the length of the list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -8,7 +8,7 @@ def element_at(my_list, idx):
     """
     if idx < 0:
         return (None)
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return (None)
     for i in range(1, len(my_list)):
         if idx == i:
