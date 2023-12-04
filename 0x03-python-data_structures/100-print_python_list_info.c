57d37f7a9ab820ece5083a69884c8798d7eb07fe
Michemor <yatoijema@gmail.com>
2023-12-04 23:00:31 +0300

    CPython #0: Python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * print_python_list_info - prints info about python list
+ * @p: pointer to python object
+ *
+ */
+void print_python_list_info(PyObject *p)
+{
+
+}
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -19,5 +19,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
 int length_list(listint_t *current);
+void print_python_list_info(PyObject *p);
 
 #endif /* LISTS_H */
