2ba2f3b6488deb179d58a293751ee036aad23cdf
Michemor <yatoijema@gmail.com>
2023-12-08 23:48:58 +0300

    counts number of keys contained in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    returns the number of keys contained in a dictionary
+    Input: a_dictionary
+    Output: number of keys
+    """
+    key_list = dict(a_dictionary)
+    return (len(key_list))
