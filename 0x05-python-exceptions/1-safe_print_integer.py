00aff1dcb134ebb087fc21c50ed64cbe021bb6a8
Michemor <yatoijema@gmail.com>
2023-12-18 19:56:23 +0300

    added check for all exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,7 +6,6 @@ def safe_print_integer(value):
     """
     try:
         print("{:d}".format(value), end="\n")
-    except ValueError:
-        return (False)
-    else:
         return (True)
+    except Exception:
+        return (False)
