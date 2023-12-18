055afc6efc4a989e1bc9672e604e7e8b7327deeb
Michemor <yatoijema@gmail.com>
2023-12-18 20:30:37 +0300

    correction on output of code

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,11 +4,10 @@ def safe_print_division(a, b):
     prints division of two numbers and checks for any
     exceptions
     """
-    result = None
     try:
         result = a / b
-        return (result)
     except Exception:
-        return (None)
+        result = None
     finally:
         print("Inside result {}".format(result))
+    return (result)
