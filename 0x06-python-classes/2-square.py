7f4f88d7f4c22d7de60b64012c9f3b773d8c844f
Michemor <yatoijema@gmail.com>
2023-12-19 21:26:15 +0300

    2. Size validation : raises exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A simple square
+
+Attributes:
+    size: defines the dimensions of a square
+"""
+
+
+class Square:
+    """
+    A simple square
+
+    """
+    def __init__(self, size=0):
+        """
+        initializes the dimensions of a square
+        as contained in size
+
+        Args:
+             size (int): width of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
