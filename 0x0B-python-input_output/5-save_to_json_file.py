6196e11e4b6affff7202c81e52bcc2dbff20ed77
Michemor <yatoijema@gmail.com>
2024-01-11 15:27:21 +0300

    serializes object and writes it to file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -10,7 +10,6 @@ def save_to_json_file(my_obj, filename):
     serializes object to json string and writes it to a file.
     Both the file and obj are passed to the function as arguments
     """
-    obj_to_str = json.dumps(my_obj)
 
     with open(filename, 'w') as f:
-        f.write(obj_to_str)
+        json.dump(my_obj, f)
