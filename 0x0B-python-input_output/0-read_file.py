9942c5d75a0e85a7aa63c8fd88d9a008455f77a4
Michemor <yatoijema@gmail.com>
2024-01-11 13:11:12 +0300

    added module documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" 
+Set of tasks that handle python python input and output
+read, open, write and close
+
+"""
 def read_file(filename=""):
     """
     opens a file passed as argument to the function and read it
