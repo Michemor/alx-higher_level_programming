f16b47493b701a7e19a5be9220a0f810e5a75fd5
Michemor <yatoijema@gmail.com>
2024-01-11 13:13:17 +0300

    pycodestyle errors

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" 
+"""
 Set of tasks that handle python python input and output
 read, open, write and close
 
 """
+
+
 def read_file(filename=""):
     """
     opens a file passed as argument to the function and read it
