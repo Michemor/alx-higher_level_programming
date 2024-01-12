650a87f2f1c70dcac5a2b0782d02eda74f357711
Michemor <yatoijema@gmail.com>
2024-01-12 17:05:43 +0300

    errors in pycodestyle fixed

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Defines a function that lists the attributes and methods of an object """
 
+
 def lookup(obj):
     """
     lists the available attributes and methods of an object
