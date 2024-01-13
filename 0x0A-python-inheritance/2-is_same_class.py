14873af96e078d95b7ce5bfe4625abd5216350bb
Michemor <yatoijema@gmail.com>
2024-01-13 21:48:49 +0300

    checks whether an object is an instance of a class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 
+module: working with inheritance in python
+and other examples of concepts in python programming
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    checks whether an object is an instance of a class
+    both object and the class are passed as arguments
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
