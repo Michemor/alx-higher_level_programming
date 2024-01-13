bdc6c19ae1e2d2b3b4bf535833c2dbe90d62392b
Michemor <yatoijema@gmail.com>
2024-01-13 22:53:29 +0300

    contains an empty class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" module: inheritance in python creating base class """
+
+
+class BaseGeometry:
+    """
+    class containing attributes and methods assosiated to geometry
+    """
+    pass
