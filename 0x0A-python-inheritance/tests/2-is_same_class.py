c56f89c4d469cb33df55d35a9d991e5043a682ea
Michemor <yatoijema@gmail.com>
2024-01-13 21:42:57 +0300

    changes made to python documentation

--- 0x0A-python-inheritance/tests/2-is_same_class.py
+++ 0x0A-python-inheritance/tests/2-is_same_class.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-""" module: inheritance in python """
+""" 
+module: working with inheritance in python
+and other examples of concepts in python programming
+"""
 
 
 def is_same_class(obj, a_class):
     """
     checks whether an object is an instance of a class
+    both object and the class are passed as arguments
     """
     if type(obj) is a_class:
         return True
