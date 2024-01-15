cf339bd3dedb1fbf078df1bd15243273a23c1cec
Michemor <yatoijema@gmail.com>
2024-01-16 00:25:37 +0300

    returns json str of a list

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ module : base class to be used to instatiate other objects """
+import json
 
 
 class Base:
     """
     Base class
-    Contains attributes and methods to be inherited 
+    Contains attributes and methods to be inherited
     by all other objects
     """
     __nb_objects = 0
@@ -22,3 +23,14 @@ class Base:
             self.id = Base. __nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        this is a static method
+        returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return (json.dumps(list_dictionaries))
