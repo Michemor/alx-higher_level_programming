ac62f84d524e2b693f0609af61983e189a7b1a4c
Michemor <yatoijema@gmail.com>
2024-06-02 20:37:07 +0300

    used wrong field in query

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all cities of california that can be found in hbtn_0d_usa database
-SELECT cities.id, cities.name FROM states, cities WHERE cities.id = states.id AND states.name = 'California' ORDER BY cities.id ASC;
+SELECT cities.id, cities.name FROM states, cities WHERE cities.state_id = states.id AND states.name = 'California' ORDER BY cities.id ASC;
