20bec66348d1791209cebcaedf15dc00375d1782
Michemor <yatoijema@gmail.com>
2024-06-17 20:07:16 +0300

    8. Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+const size = parseInt(argv[2]);
+
+if (size) {
+  for(let i = 0; i < size; i++) {
+    let symbol = "";
+    for (let j = 0; j < size; j++) {
+      symbol += 'X';
+  }
+    console.log(symbol);
+ }
+} else {
+  console.log('Missing size');
+}
