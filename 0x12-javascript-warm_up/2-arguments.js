65187849437172fde61b30427e876868dfafd6d7
Michemor <yatoijema@gmail.com>
2024-06-17 23:29:31 +0300

    Correction of "2. Arguments"

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const { argv } = require('process');
-const argvlength = argv.length - 2;
 
+const argvlength = process.argv.length - 2;
 if (argvlength <= 0) {
   console.log('No argument');
 } else if (argvlength === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
-}
+};
