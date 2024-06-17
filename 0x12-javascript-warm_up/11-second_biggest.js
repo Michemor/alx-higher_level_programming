1b965f3e3c2ddba8d3ce8e31db2a855cc4c1a84e
Michemor <yatoijema@gmail.com>
2024-06-17 23:16:45 +0300

    used process directly without destructuring

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 
-const { argv } = require('process');
 let largest = 0;
 let secondLargest = 0;
-
-if (argv.length > 3) {
-  for (let i = 2; i < argv.length; i++) {
-    const num = parseInt(argv[i], 10);
-
+if (process.argv.length > 3) {
+  for (let i = 2; i < process.argv.length; i++) {
+    const num = parseInt(process.argv[i], 10);
     if (num > largest) {
       secondLargest = largest;
       largest = num;
