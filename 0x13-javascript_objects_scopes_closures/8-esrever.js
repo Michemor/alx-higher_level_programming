09781cbc94a7faefb6b3a6c47a5a687bb49ce4bc
Michemor <yatoijema@gmail.com>
2024-06-18 17:41:48 +0300

    8. Esrever

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let reverseList = [];
+  let j = 0;
+  for (let i = list.length - 1; i >= 0; i--)
+  {
+    reverseList[j++] = list[i];
+  }
+  return reverseList;
+}
