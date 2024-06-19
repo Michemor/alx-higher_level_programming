de785042f360e3ad33e5a35c822662f0b5529441
Michemor <yatoijema@gmail.com>
2024-06-19 11:32:18 +0300

    Correction of "3. Value of my argument"

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,8 +2,8 @@
 
 const { argv } = require('process');
 
-if (argv.length <= 2) {
-  console.log('No argument');
-} else {
+if (argv[2]) {
   console.log(argv[2]);
+} else {
+  console.log('No argument');
 }
