9615aeb4622e29dd1c81ed28d2bd1b13b96525f6
Michemor <yatoijema@gmail.com>
2024-06-23 16:13:11 +0300

    Correction of "7. All states via SQLAlchemy"

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,12 +4,14 @@
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                                                            argv[1],
-                                                            argv[2],
-                                                            argv[3]))
-query = select(State).order_by(State.id)
-with engine.connect() as conn:
-    for row in conn.execute(query):
-        print(f"{row.id} : {row.name}")
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                            .format(argv[1], argv[2],argv[3]),
+                            pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    stmt = session.query(State)
+    for row in stmt:
+            print(f"{row.id} : {row.name}")
