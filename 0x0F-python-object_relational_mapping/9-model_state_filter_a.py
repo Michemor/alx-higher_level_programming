3dd9c510526d093c1dabc307a1b441ef3b0e3cb8
Michemor <yatoijema@gmail.com>
2024-06-23 17:42:37 +0300

    9. Contains `a`

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa
+"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import select, create_engine
+import sys
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    stmt = session.query(State).filter(State.name.like('%a%'))
+    .order_by(State.id)
+    for row in stmt:
+        print(f"{row.id}: {row.name}")
