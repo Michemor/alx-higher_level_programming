e2673219831d7f8bce838a1d8d86f375030f9484
Michemor <yatoijema@gmail.com>
2024-07-05 01:12:21 +0300

    Correction of "6. Find a peak"

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,19 +7,12 @@ def find_peak(list_of_integers):
     takes a list of integers as an argument
     finds the peak; largest number in the list
     """
-    list_len = len(list_of_integers)
-    if list_len <= 0:
+    if len(list_of_integers) == 0:
         return None
 
-    for i in range(list_len):
-        sorted = True
-        for j in range(list_len - i - 1):
-            if (list_of_integers[j] > list_of_integers[j + 1]):
-                temp = list_of_integers[j]
-                list_of_integers[j] = list_of_integers[j + 1]
-                list_of_integers[j + 1] = temp
-                sorted = False
-        if sorted:
-            break
+    peak = 0
+    for i in list_of_integers:
+        if (i > peak):
+            peak = i
 
-    return (list_of_integers[list_len - 1])
+    return (peak)
