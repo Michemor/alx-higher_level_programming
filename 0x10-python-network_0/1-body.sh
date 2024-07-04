e1ddb7d94fed68d269e94325ac9004823ceb66eb
Michemor <yatoijema@gmail.com>
2024-07-04 22:58:20 +0300

    Correction of "1. cURL to the end"

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# displays the body of a 200 status code response
-curl -sI "$1"
+# displays the body of a 200 status code http response
+if [ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -ne "200" ]; then echo "Route 2"; fi
