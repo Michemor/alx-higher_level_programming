bbbac6ef4eeb794ca3d1460657e31a286ef2c60b
Michemor <yatoijema@gmail.com>
2024-07-05 01:19:44 +0300

    Correction of "6. Find a peak"

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# finds the peak in a list of unsorted intergers
+"""
+function that finds the peak in a list of unsorted intergers
+"""
 
 
 def find_peak(list_of_integers):
@@ -7,12 +9,13 @@ def find_peak(list_of_integers):
     takes a list of integers as an argument
     finds the peak; largest number in the list
     """
+    if list_of_integers is None:
+        return None
     if len(list_of_integers) == 0:
         return None
-
     peak = 0
     for i in list_of_integers:
-        if (i > peak):
+        if i > peak:
             peak = i
 
     return (peak)
