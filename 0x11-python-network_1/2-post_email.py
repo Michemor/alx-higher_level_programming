af07da36318ac6204cc8386754090cf391d20c69
Michemor <yatoijema@gmail.com>
2024-07-05 17:07:37 +0300

    Correction of "2. POST an email #0"

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,9 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     email = urlencode({'email': argv[2]})
-    email = email.encode('utf-8')
+    email = email.encode('ascii')
     req = Request(url, email)
     with urlopen(req) as response:
-        body = response.read()
-        print(body.decode('utf-8'))
-
+        body = response.read().decode('utf-8')
