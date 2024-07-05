763eaf17cb6938d88fc84a3c4bf1d4002e993384
Michemor <yatoijema@gmail.com>
2024-07-05 23:09:24 +0300

    Correction of "9. My GitHub!"

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,14 +6,10 @@ Uses the GitHub API to display your id
 
 import requests
 from sys import argv
+from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-
-    url = f"https://api.github.com/users/{username}"
-    headers = {"Accept": "application/vnd.github+json",
-                "Authorization": f"{password}",
-                "X-Github-Api-Version": "2022-11-28"}
-    response = requests.get(url, headers=headers)
+    credentials = HTTPBasicAuth(argv[1], argv[2])
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=credentials)
     print(response.json().get("id"))
