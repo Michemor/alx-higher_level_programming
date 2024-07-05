6ebbc7cec2e922154a732cb6f92cac147cc4a37a
Michemor <yatoijema@gmail.com>
2024-07-05 20:41:19 +0300

    Correction of "4. What's my status? #1"

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@
 import requests
 
 if __name__ == "__main__":
-    rep = requests.get('https://alx-intranet.hbtn.io/status')
-    print("Body response: ")
+    rep = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
     print("\t- type: {}".format(type(rep.text)))
     print("\t- content: {}".format(rep.text))
