0f2e00d947a3e5fe7faaae837f4cfd13de6f98a8
Michemor <yatoijema@gmail.com>
2024-07-05 23:24:05 +0300

    Correction of "2. POST an email #0"

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""  sends a POST request to the passed URL with the email as a parameter,"""
+"""
+sends a POST request to the passed URL
+the email is passed as a parameter
+"""
 
 from urllib.request import urlopen, Request
 from sys import argv
+from urllib.parse import urlencode
 
 if __name__ == "__main__":
     url = argv[1]
-    val = {"email": argv[2]}
-    email = urlencode(val).encode("ascii")
-
+    data = {"email": argv[2]}
+    email = urlencode(data).encode("ascii")
     req = Request(url, email)
     with urlopen(req) as response:
         body = response.read().decode('utf-8')
