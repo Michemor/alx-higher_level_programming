3f82801a2c17ea42b17708e6af84096e76acd377
Michemor <yatoijema@gmail.com>
2024-08-07 18:02:32 +0300

    4. Toggle classes

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+  $('div#toggle_header').click(() => {
+    if($('header').hasClass('red')) {
+        $('header').removeClass('red');
+        $('header').addClass('green');
+    } else {
+        $('header').removeClass('green');
+        $('header').addClass('red');
+    }
+  });
+});
