c3f77bb6b5e899a1b311a9f93c63c12aefd980fe
Michemor <yatoijema@gmail.com>
2024-08-08 18:56:19 +0300

    task 101

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,2 +1,4 @@
-const colorRed = document.querySelector('header');
-colorRed.style.color = '#FF0000';
+document.addEventListener('DOMContentLoaded', () => {
+  const colorRed = document.querySelector('header');
+  colorRed.style.color = '#FF0000';
+});
